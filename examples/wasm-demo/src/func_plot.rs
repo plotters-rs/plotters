11151bacfcd0921df9485583548cffae6c7a42d2
Hao Hou <haohou302@gmail.com>
2019-05-13 15:03:37 -0600

    Update example

---
+++ examples/wasm-demo/src/func_plot.rs
@@ -0,0 +1,32 @@
+use plotters::prelude::*;
+use wasm_bindgen::prelude::*;
+
+fn start_plotting(element: &str, pow: i32) -> Result<(), Box<dyn std::error::Error>> {
+    let mut backend = CanvasBackend::new(element).unwrap();
+    backend.open()?;
+    let root: DrawingArea<_, _> = backend.into();
+    let font = Into::<FontDesc>::into("Arial").resize(20.0);
+    root.fill(&White)?;
+    let mut chart = ChartBuilder::on(&root)
+        .caption(format!("y=x^{}", pow), &font)
+        .x_label_area_size(30)
+        .y_label_area_size(30)
+        .build_ranged(-1f32..1f32, -1.2f32..1.2f32);
+
+    chart.configure_mesh().x_labels(3).y_labels(3).draw()?;
+
+    chart.draw_series(LineSeries::new(
+        (-50..=50)
+            .map(|x| x as f32 / 50.0)
+            .map(|x| (x, x.powf(pow as f32))),
+        &Red,
+    ))?;
+
+    root.close()?;
+    return Ok(());
+}
+
+#[wasm_bindgen]
+pub fn draw_func(element: &str, p: i32) -> bool {
+    return start_plotting(element, p).is_ok();
+}
---
+++ examples/wasm-demo/src/mandelbrot.rs
@@ -0,0 +1,71 @@
+use plotters::prelude::*;
+use wasm_bindgen::prelude::*;
+use std::ops::Range;
+
+fn mandelbrot_set(
+    real: Range<f64>,
+    complex: Range<f64>,
+    samples: (usize, usize),
+    max_iter: usize,
+) -> impl Iterator<Item = (f64, f64, usize)> {
+    let step = (
+        (real.end - real.start) / samples.0 as f64,
+        (complex.end - complex.start) / samples.1 as f64,
+    );
+    return (0..(samples.0 * samples.1)).map(move |k| {
+        let c = (
+            real.start + step.0 * (k % samples.0) as f64,
+            complex.start + step.1 * (k / samples.0) as f64,
+        );
+        let mut z = (0.0, 0.0);
+        let mut cnt = 0;
+        while cnt < max_iter && z.0 * z.0 + z.1 * z.1 <= 1e10 {
+            z = (z.0 * z.0 - z.1 * z.1 + c.0, 2.0 * z.0 * z.1 + c.1);
+            cnt += 1;
+        }
+        return (c.0, c.1, cnt);
+    });
+}
+
+fn draw_mandelbrot_impl(element: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let mut backend = CanvasBackend::new(element).unwrap();
+    backend.open()?;
+    
+    let root: DrawingArea<_, _> = backend.into();
+    root.fill(&White)?;
+
+    let mut chart = ChartBuilder::on(&root)
+        .margin(20)
+        .x_label_area_size(10)
+        .y_label_area_size(10)
+        .build_ranged(-2.1f64..0.6f64, -1.2f64..1.2f64);
+
+    chart
+        .configure_mesh()
+        .disable_x_mesh()
+        .disable_y_mesh()
+        .draw()?;
+
+    let plotting_area = chart.plotting_area();
+
+    let range = plotting_area.get_pixel_range();
+    let (pw, ph) = (range.0.end - range.0.start, range.1.end - range.1.start);
+    let (xr, yr) = (chart.x_range(), chart.y_range());
+
+    for (x, y, c) in mandelbrot_set(xr, yr, (pw as usize, ph as usize), 100) {
+        if c != 100 {
+            plotting_area
+                .draw_pixel((x, y), &HSLColor(c as f64/100.0, 1.0, 0.5))?;
+        } else {
+            plotting_area.draw_pixel((x, y), &Black)?;
+        }
+    }
+
+    root.close()?;
+    return Ok(());
+}
+
+#[wasm_bindgen]
+pub fn draw_mandelbrot(element: &str) -> bool {
+    return draw_mandelbrot_impl(element).is_ok();
+}
