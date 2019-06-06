3725e585220a89b2a11f1c8df1a1dc13a9716676
Hao Hou <haohou302@gmail.com>
2019-06-06 01:04:00 -0600

    Changelog && fmt && rename

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,7 @@
 - ChartContext now support drawing axis description. Use `MeshStyle::x_desc` and `MeshStyle::y_desc` to specify the axis description text.
 - Add series label support. `ChartContext::draw_series` now returns a struct `SeriesAnno` that collects the additional information for series labeling. `ChartContext::draw_series_labels` are used to actually draw the series label. (See `examples/chart.rs` for detailed examples)
 - Mocking drawing backend.
+- evcxr Support
 
 ### Improvement
 - Unify `OwnedText` and `Text` into `Text`. Previously, `OwnedText` and `Text` are two seperate types, one holds a `String` another holds a `&str`. Now `OwnedText` is removed.
--- src/evcxr.rs
+++ src/evcxr.rs
@@ -1,16 +1,26 @@
-use crate::drawing::{SVGBackend , DrawingArea, IntoDrawingArea};
 use crate::coord::Shift;
+use crate::drawing::{DrawingArea, IntoDrawingArea, SVGBackend};
 
+/// The wrapper for the generated SVG
 pub struct SVGWrapper(Vec<u8>);
 
 impl SVGWrapper {
     pub fn evcxr_display(&self) {
         let svg = String::from_utf8_lossy(self.0.as_slice());
-        println!("EVCXR_BEGIN_CONTENT text/html\n<svg>{}</svg>\nEVCXR_END_CONTENT", svg);
+        println!(
+            "EVCXR_BEGIN_CONTENT text/html\n<svg>{}</svg>\nEVCXR_END_CONTENT",
+            svg
+        );
     }
 }
 
-pub fn start_render<Draw: FnOnce(DrawingArea<SVGBackend, Shift>) -> Result<(), Box<dyn std::error::Error>>>(size:(u32, u32), draw: Draw) -> SVGWrapper {
+/// Start drawing an evcxr figure
+pub fn evcxr_figure<
+    Draw: FnOnce(DrawingArea<SVGBackend, Shift>) -> Result<(), Box<dyn std::error::Error>>,
+>(
+    size: (u32, u32),
+    draw: Draw,
+) -> SVGWrapper {
     let mut buffer = vec![];
     let root = SVGBackend::with_buffer(&mut buffer, size).into_drawing_area();
     draw(root).expect("Drawing failure");
