77f6da00d72a97006f1dde8582103ab274764ad8
Hao Hou <haohou302@gmail.com>
2019-09-08 15:22:57 -0600

    Make start-server.sh for WASM demo works for debug enabled

--- examples/wasm-demo/start-server.sh
+++ examples/wasm-demo/start-server.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 set -e
+
 CONFIG=release
 mkdir -p www/pkg
+
 rustup target add wasm32-unknown-unknown
+
 if [ -z "$(cargo install --list | grep wasm-bindgen-cli)" ]
 then
 	cargo install wasm-bindgen-cli
 fi
-cargo build --target=wasm32-unknown-unknown --release
+
+if [ "${CONFIG}" = "release" ]
+then
+	cargo build --target=wasm32-unknown-unknown --release
+else
+	cargo build --target=wasm32-unknown-unknown --release
+fi
+
 wasm-bindgen --out-dir www/pkg   --target web target/wasm32-unknown-unknown/${CONFIG}/wasm_demo.wasm
 cd www
+
 echo "Goto http://localhost:8000/ to see the result"
 python ../server.py
