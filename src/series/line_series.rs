9c65e65a53723180d0c260a7ea4f969b8bcca384
Hao Hou <haohou302@gmail.com>
2019-10-04 21:17:53 -0600

    Add unit test

--- src/series/line_series.rs
+++ src/series/line_series.rs
@@ -32,3 +32,37 @@ impl<Coord, I: IntoIterator<Item = Coord>> LineSeries<Coord, I> {
         }
     }
 }
+
+#[cfg(test)]
+mod test {
+    use crate::prelude::*;
+
+    #[test]
+    fn test_line_series() {
+        let drawing_area = create_mocked_drawing_area(200, 200, |m| {
+            m.check_draw_path(|c, s, path| {
+                assert_eq!(c, RED.to_rgba());
+                assert_eq!(s, 3);
+                for i in 0..100 {
+                    assert_eq!(path[i], (i as i32 * 2, 200 - i as i32 * 2));
+                }
+            });
+
+            m.drop_check(|b| {
+                assert_eq!(b.num_draw_path_call, 1);
+                assert_eq!(b.draw_count, 1);
+            });
+        });
+
+        let mut chart = ChartBuilder::on(&drawing_area)
+            .build_ranged(0..100, 0..100)
+            .expect("Build chart error");
+
+        chart
+            .draw_series(LineSeries::new(
+                (0..100).map(|x| (x, x)),
+                Into::<ShapeStyle>::into(&RED).stroke_width(3),
+            ))
+            .expect("Drawing Error");
+    }
+}
