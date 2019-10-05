97201bf5412d4bddaee5f28a8b4b827a4f35c210
Hao Hou <haohou302@gmail.com>
2019-10-04 21:56:55 -0600

    More test

--- src/chart/context.rs
+++ src/chart/context.rs
@@ -451,3 +451,29 @@ impl<'a, DB: DrawingBackend, X: Ranged, Y: Ranged> ChartContext<'a, DB, RangedCo
         DualCoordChartContext::new(self, RangedCoord::new(x_coord, y_coord, pixel_range))
     }
 }
+
+#[cfg(test)]
+mod test {
+    use crate::prelude::*;
+
+    #[test]
+    fn test_chart_context() {
+        let drawing_area = create_mocked_drawing_area(200, 200, |m| {});
+
+        let mut chart = ChartBuilder::on(&drawing_area)
+            .caption("Test Title", ("Arial", 10))
+            .x_label_area_size(20)
+            .y_label_area_size(20)
+            .set_label_area_size(LabelAreaPosition::Top, 20)
+            .set_label_area_size(LabelAreaPosition::Bottom, 20)
+            .build_ranged(0..10, 0..10)
+            .expect("Create chart")
+            .set_secondary_coord(0.0..1.0, 0.0..1.0);
+
+        chart.configure_mesh().draw().expect("Draw mesh");
+        chart
+            .configure_secondary_axes()
+            .draw()
+            .expect("Draw Secondary axes");
+    }
+}
