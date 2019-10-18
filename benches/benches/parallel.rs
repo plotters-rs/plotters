3a17f8b7a3e6d1c2ae1d2c40f584313f491251c6
Hao Hou <haohou302@gmail.com>
2019-10-18 17:48:04 -0600

    Add more parallel benches

--- benches/benches/parallel.rs
+++ benches/benches/parallel.rs
@@ -79,8 +79,70 @@ fn draw_func_4x4_parallel_and_blit(c: &mut Criterion) {
     });
 }
 
+fn draw_func_2x1_parallel_and_blit(c: &mut Criterion) {
+    let mut buffer = vec![0; (W * H * 3) as usize];
+    c.bench_function("parallel::draw_func_2x1_parallel_and_blit", |b| {
+        b.iter(|| {
+            let root = BitMapBackend::with_buffer(&mut buffer, (W, H)).into_drawing_area();
+            let areas = root.split_evenly((2,1));
+            let mut elements: Vec<_> = areas
+                .iter()
+                .map(|area| area.dim_in_pixel())
+                .map(|size| BitMapElement::new((0, 0), size))
+                .collect();
+
+            elements
+                .par_iter_mut()
+                .for_each(|e| draw_plot(&e.as_bitmap_backend().into_drawing_area(), 2.0));
+
+            areas
+                .into_iter()
+                .zip(elements.into_iter())
+                .for_each(|(a, e)| a.draw(&e).unwrap());
+        })
+    });
+}
+
+fn draw_func_2x1_inplace_parallel(c: &mut Criterion) {
+    let mut buffer = vec![0u8; (W * H * 3) as usize];
+    c.bench_function("parallel::draw_func_2x1_inplace", |b| {
+        b.iter(|| {
+            let (upper, lower) = unsafe {
+                let upper_addr = &mut buffer[0] as *mut u8;
+                let lower_addr = &mut buffer[(W * H * 3 / 2) as usize] as *mut u8;
+                (
+                    std::slice::from_raw_parts_mut(upper_addr, (W * H * 3 / 2) as usize),
+                    std::slice::from_raw_parts_mut(lower_addr, (W * H * 3 / 2) as usize),
+                )
+            };
+
+            [upper, lower]
+                .par_iter_mut()
+                .for_each(|b| draw_plot(&BitMapBackend::with_buffer(*b, (W, H / 2)).into_drawing_area(), 2.0));
+        })
+    });
+}
+
+fn draw_func_2x1_seq(c: &mut Criterion) {
+    let mut buffer = vec![0u8; (W * H * 3) as usize];
+    c.bench_function("parallel::draw_func_2x1_seq", |b| {
+        b.iter(|| {
+            let root = BitMapBackend::with_buffer(&mut buffer, (W, H)).into_drawing_area();
+            root.split_evenly((2,1))
+                .iter_mut()
+                .for_each(|area| draw_plot(area, 2.0));
+        })
+    });
+}
+
 criterion_group! {
     name = parallel_group;
     config = Criterion::default().sample_size(10);
-    targets = draw_func_1x1_seq, draw_func_4x4_seq, draw_func_4x4_parallel_and_blit
+    targets = 
+        draw_func_1x1_seq, 
+        draw_func_4x4_seq, 
+        draw_func_4x4_parallel_and_blit,
+        draw_func_2x1_parallel_and_blit,
+        draw_func_2x1_inplace_parallel,
+        draw_func_2x1_seq
 }
