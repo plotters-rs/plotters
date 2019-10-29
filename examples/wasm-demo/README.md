9005cd76a0d1af1684d64ba81b5e4408a5a587f1
TatriX <tatrics@gmail.com>
2019-10-29 12:24:28 +0100

    Refactor wasm-demo to better support mobiles

--- examples/wasm-demo/README.md
+++ examples/wasm-demo/README.md
@@ -1,12 +1,19 @@
 # The Minimal Demo for Plotters + WASM
 
-Run the demo locally with the following command under this directory.
+You can run the demo locally using `npm`:
+```bash
+cd www
+npm install
+npm start
+```
+
+or with the following command under this directory (Python required).
 
 ```
 ./start-server.sh
 ```
 
-For Windows user doesn't have Bash, `start-server.bat` can be used to launch the server.
+For Windows users without Bash, `start-server.bat` can be used to launch the server.
 
 ```
 start-server.bat
--- examples/wasm-demo/www/index.js
+++ examples/wasm-demo/www/index.js
@@ -2,50 +2,58 @@ import { Chart, drawPower } from "wasm-demo";
 
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-
-// Setup canvas to properly handle high DPI.
-canvas.style.width = canvas.width + "px";
-canvas.style.height = canvas.height + "px";
-const dpr = window.devicePixelRatio || 1;
-const rect = canvas.getBoundingClientRect();
-canvas.width *= dpr;
-canvas.height *= dpr;
-ctx.scale(dpr, dpr);
-
 const coord = document.getElementById("coord");
-
-let chart = undefined;
-window.addEventListener("mousemove", (event) => {
-	if (chart === undefined) {
-        return;
-    }
-
-    const point = chart.coord(event.offsetX, event.offsetY);
-
-    coord.innerText = (point)
-        ? `(${point.x.toFixed(3)}, ${point.y.toFixed(3)})`
-        : "Mouse pointer is out of range";
-});
-
-const select = document.getElementById("pow");
+const plotType = document.getElementById("plot-type");
 const status = document.getElementById("status");
+let chart = undefined;
 
-const updatePlot = () => {
-    const selected = select.selectedOptions[0];
+main();
+
+function main() {
+    setupUI();
+    setupCanvas();
+}
+
+/** Add event listeners */
+function setupUI() {
+    status.innerText = "WebAssembly loaded!";
+    plotType.addEventListener("change", updatePlot);
+    window.addEventListener("resize", setupCanvas);
+    window.addEventListener("mousemove", onMouseMove);
+}
+
+/** Setup canvas to properly handle high DPI and redraw current plot. */
+function setupCanvas() {
+    const dpr = window.devicePixelRatio || 1;
+    const aspectRatio = canvas.width / canvas.height;
+    const size = Math.min(canvas.width, canvas.parentNode.offsetWidth);
+    canvas.style.width = size + "px";
+    canvas.style.height = size / aspectRatio + "px";
+    canvas.width = size * dpr;
+    canvas.height = size / aspectRatio * dpr;
+    ctx.scale(dpr, dpr);
+    updatePlot();
+}
+
+/** Update displayed coordinates */
+function onMouseMove(event) {
+	if (chart) {
+        const point = chart.coord(event.offsetX, event.offsetY);
+        coord.innerText = (point)
+            ? `(${point.x.toFixed(3)}, ${point.y.toFixed(3)})`
+            : "Mouse pointer is out of range";
+    }
+}
+
+/** Redraw currently selected plot */
+function updatePlot() {
+    const selected = plotType.selectedOptions[0];
 	status.innerText = `Rendering ${selected.innerText}...`;
-	setTimeout(() => {
-		chart = undefined;
-		const start = performance.now();
-        chart = (selected.value == "mandelbrot")
-            ? Chart.mandelbrot(canvas)
-            : Chart.power("canvas", Number(selected.value));
-		const end = performance.now();
-		status.innerText = `Rendered ${selected.innerText} in ${end - start}ms`;
-	}, 5);
-};
-
-status.innerText = "WebAssembly loaded!";
-
-updatePlot();
-
-select.addEventListener("change", updatePlot);
+	chart = undefined;
+	const start = performance.now();
+    chart = (selected.value == "mandelbrot")
+        ? Chart.mandelbrot(canvas)
+        : Chart.power("canvas", Number(selected.value));
+	const end = performance.now();
+	status.innerText = `Rendered ${selected.innerText} in ${Math.ceil(end - start)}ms`;
+}
