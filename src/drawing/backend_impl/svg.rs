3659b0c0d787a5d2b22a601dff28ddf0ef59ff83
René Rössler <rene@freshx.de>
2020-03-16 12:21:25 +0100

    set svg viewBox

--- src/drawing/backend_impl/svg.rs
+++ src/drawing/backend_impl/svg.rs
@@ -124,6 +124,7 @@ impl<'a> SVGBackend<'a> {
             &[
                 ("width", &format!("{}", size.0)),
                 ("height", &format!("{}", size.1)),
+                ("viewBox", &format!("0 0 {} {}", size.0, size.1)),
                 ("xmlns", "http://www.w3.org/2000/svg"),
             ],
             false,
