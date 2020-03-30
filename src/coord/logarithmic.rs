869f6faee2a55e7d69daf04136484bee402ff3ca
René Rössler <rene@freshx.de>
2020-03-30 18:20:47 +0200

    fix

--- src/coord/logarithmic.rs
+++ src/coord/logarithmic.rs
@@ -97,8 +97,8 @@ impl<V: LogScalable> Ranged for LogCoord<V> {
             let to = self.logic.end.as_f64().ceil() as i32;
             let mut ret = vec![];
             { from..=to }
-                .step_by((f64::from(to - from) / f64::from(max_points)).ceil() as i32)
-                .for_each(|i| ret.push(V::from_f64(f64::from(i))));
+            .step_by((f64::from(to - from) / f64::from(max_points as u32)).ceil() as usize)
+            .for_each(|i| ret.push(V::from_f64(f64::from(i))));
             return ret;
         }
         let tier_2_density = if max_points < tier_1 {
