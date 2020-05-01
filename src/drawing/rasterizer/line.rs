cacdcb7eb7869e89e48bdec269acc90d9d2ced35
Alistair Francis <alistair@alistair23.me>
2020-05-01 01:35:11 -0700

    Fixup some clippy warnings
    Signed-off-by: Alistair Francis <alistair@alistair23.me>

--- src/drawing/rasterizer/line.rs
+++ src/drawing/rasterizer/line.rs
@@ -112,7 +112,7 @@ pub fn draw_line<DB: DrawingBackend, S: BackendStyle>(
         y += grad;
     }
 
-    if to.0 >= batch_limit + 1 && y < f64::from(to.1) {
+    if to.0 > batch_limit && y < f64::from(to.1) {
         let x = batch_limit as i32 + 1;
         if 1.0 + y.floor() - y > 1e-5 {
             check_result!(put_pixel((x, y as i32), 1.0 + y.floor() - y));
