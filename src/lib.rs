04d7b24d36c38ce0e368d16ef413bca818a0365f
Hao Hou <haohou302@gmail.com>
2020-09-02 18:44:55 -0600

    Update metadata

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,12 @@
+/*!
+   The Plotters bitmap backend. 
+   
+   The plotters bitmap backend allows you to render images by Plotters into bitmap. 
+   You can either generate image file(PNG, JPG, GIF, etc) or rendering the bitmap within internal buffer (for example for framebuffer, etc). 
+
+   See the documentation for [BitMapBackend](struct.BitMapBackend.html) for more details.
+*/
+
 mod bitmap;
 pub use bitmap::BitMapBackend;
 
