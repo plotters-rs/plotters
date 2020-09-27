91d8a9923233e892ccca99c37636f519cc66d422
TheEdward162 <TheEdward162@gmail.com>
2020-09-27 11:20:02 +0200

    fix UB in minifb example
    minifb example was incorrectly casting Vec<u8> to &[u32]

--- examples/minifb-demo/src/main.rs
+++ examples/minifb-demo/src/main.rs
@@ -5,11 +5,48 @@ use plotters_bitmap::BitMapBackend;
 use std::collections::VecDeque;
 use std::error::Error;
 use std::time::SystemTime;
+use std::borrow::{Borrow, BorrowMut};
 const W: usize = 480;
 const H: usize = 320;
 
 const SAMPLE_RATE: f64 = 10_000.0;
 const FREAME_RATE: f64 = 30.0;
+
+struct BufferWrapper(Vec<u32>);
+impl Borrow<[u8]> for BufferWrapper {
+    fn borrow(&self) -> &[u8] {
+        // Safe for alignment: align_of(u8) <= align_of(u32)
+        // Safe for cast: u32 can be thought of as being transparent over [u8; 4]
+        unsafe {
+            std::slice::from_raw_parts(
+                self.0.as_ptr() as *const u8,
+                self.0.len() * 4
+            )
+        }
+    }
+}
+impl BorrowMut<[u8]> for BufferWrapper {
+    fn borrow_mut(&mut self) -> &mut [u8] {
+        // Safe for alignment: align_of(u8) <= align_of(u32)
+        // Safe for cast: u32 can be thought of as being transparent over [u8; 4]
+        unsafe {
+            std::slice::from_raw_parts_mut(
+                self.0.as_mut_ptr() as *mut u8,
+                self.0.len() * 4
+            )
+        }
+    }
+}
+impl Borrow<[u32]> for BufferWrapper {
+    fn borrow(&self) -> &[u32] {
+        self.0.as_slice()
+    }
+}
+impl BorrowMut<[u32]> for BufferWrapper {
+    fn borrow_mut(&mut self) -> &mut [u32] {
+        self.0.as_mut_slice()
+    }
+}
 
 fn get_window_title(fx: f64, fy: f64, iphase: f64) -> String {
     format!(
@@ -19,7 +56,7 @@ fn get_window_title(fx: f64, fy: f64, iphase: f64) -> String {
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut buf = vec![0u8; W * H * 4];
+    let mut buf = BufferWrapper(vec![0u32; W * H]);
 
     let mut fx: f64 = 1.0;
     let mut fy: f64 = 1.1;
@@ -33,7 +70,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         WindowOptions::default(),
     )?;
     let root =
-        BitMapBackend::<BGRXPixel>::with_buffer_and_format(&mut buf[..], (W as u32, H as u32))?
+        BitMapBackend::<BGRXPixel>::with_buffer_and_format(buf.borrow_mut(), (W as u32, H as u32))?
             .into_drawing_area();
     root.fill(&BLACK)?;
 
@@ -81,7 +118,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 
         if epoch - last_flushed > 1.0 / FREAME_RATE {
             let root = BitMapBackend::<BGRXPixel>::with_buffer_and_format(
-                &mut buf[..],
+                buf.borrow_mut(),
                 (W as u32, H as u32),
             )?
             .into_drawing_area();
@@ -134,8 +171,8 @@ fn main() -> Result<(), Box<dyn Error>> {
                     break;
                 }
             }
-            let buf = unsafe { std::slice::from_raw_parts(&buf[0] as *const _ as *const _, H * W) };
-            window.update_with_buffer(buf)?;
+
+            window.update_with_buffer(buf.borrow())?;
             last_flushed = epoch;
         }
 
