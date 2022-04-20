3e3941b9d472cbbc1f538c5c6db7c7573e939962
Aaron Erhardt <aaron.erhardt@t-online.de>
2022-04-20 14:44:57 +0200

    Fix doc tests
    Signed-off-by: Aaron Erhardt <aaron.erhardt@t-online.de>

--- src/lib.rs
+++ src/lib.rs
@@ -470,10 +470,9 @@ Besides that, the drawing area also allows the customized coordinate system, by 
 ```rust
 use plotters::prelude::*;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let root_drawing_area =
-        BitMapBackend::new("plotters-doc-data/2.png", (300, 200)).into_drawing_area();
+    let root = BitMapBackend::new("plotters-doc-data/2.png", (300, 200)).into_drawing_area();
     // And we can split the drawing area into 3x3 grid
-    let child_drawing_areas = root_drawing_area.split_evenly((3, 3));
+    let child_drawing_areas = root.split_evenly((3, 3));
     // Then we fill the drawing area with different color
     for (area, color) in child_drawing_areas.into_iter().zip(0..) {
         area.fill(&Palette99::pick(color))?;
