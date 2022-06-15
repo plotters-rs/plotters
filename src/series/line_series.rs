c5c5cc5c06f6c2bb6134ca990bd512e619647d97
Fabio A. Correa <facorread@gmail.com>
2022-06-15 11:14:07 -0400

    LineSeries

--- src/series/line_series.rs
+++ src/series/line_series.rs
@@ -3,8 +3,32 @@ use crate::style::ShapeStyle;
 use plotters_backend::DrawingBackend;
 use std::marker::PhantomData;
 
-/// The line series object, which takes an iterator of points in guest coordinate system
-/// and creates the element rendering the line plot
+/**
+The line series object, which takes an iterator of data points in guest coordinate system
+and creates appropriate lines and points with the given style.
+
+# Example
+
+```
+use plotters::prelude::*;
+let x_values = [0.0f64, 1., 2., 3., 4.];
+let drawing_area = SVGBackend::new("line_series_point_size.svg", (300, 200)).into_drawing_area();
+drawing_area.fill(&WHITE).unwrap();
+let mut chart_builder = ChartBuilder::on(&drawing_area);
+chart_builder.margin(10).set_left_and_bottom_label_area_size(20);
+let mut chart_context = chart_builder.build_cartesian_2d(0.0..4.0, 0.0..3.0).unwrap();
+chart_context.configure_mesh().draw().unwrap();
+chart_context.draw_series(LineSeries::new(x_values.map(|x| (x, 0.3 * x)), BLACK)).unwrap();
+chart_context.draw_series(LineSeries::new(x_values.map(|x| (x, 2.5 - 0.05 * x * x)), RED)
+    .point_size(5)).unwrap();
+chart_context.draw_series(LineSeries::new(x_values.map(|x| (x, 2. - 0.1 * x * x)), BLUE.filled())
+    .point_size(4)).unwrap();
+```
+
+The result is a chart with three line series; two of them have their data points highlighted:
+
+![](https://cdn.jsdelivr.net/gh/facorread/plotters-doc-data@64e0a28/apidoc/line_series_point_size.svg)
+*/
 pub struct LineSeries<DB: DrawingBackend, Coord> {
     style: ShapeStyle,
     data: Vec<Coord>,
@@ -35,6 +59,11 @@ impl<DB: DrawingBackend, Coord: Clone + 'static> Iterator for LineSeries<DB, Coo
 }
 
 impl<DB: DrawingBackend, Coord> LineSeries<DB, Coord> {
+    /**
+    Creates a new line series based on a data iterator and a given style.
+
+    See [`LineSeries`] for more information and examples.
+    */
     pub fn new<I: IntoIterator<Item = Coord>, S: Into<ShapeStyle>>(iter: I, style: S) -> Self {
         Self {
             style: style.into(),
@@ -45,6 +74,11 @@ impl<DB: DrawingBackend, Coord> LineSeries<DB, Coord> {
         }
     }
 
+    /**
+    Sets the size of the points in the series, in pixels.
+
+    See [`LineSeries`] for more information and examples.
+    */
     pub fn point_size(mut self, size: u32) -> Self {
         self.point_size = size;
         self
