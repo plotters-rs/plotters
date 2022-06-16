81207337c28304fd154c5416478752e68fe090b7
Fabio A. Correa <facorread@gmail.com>
2022-06-16 11:43:10 -0400

    Histogram apidocs

--- src/series/histogram.rs
+++ src/series/histogram.rs
@@ -16,7 +16,67 @@ pub struct Horizontal;
 impl HistogramType for Vertical {}
 impl HistogramType for Horizontal {}
 
-/// The series that aggregate data into a histogram
+/**
+Presents data in a histogram. Input data can be raw or aggregated.
+
+# Examples
+
+```
+use plotters::prelude::*;
+let data = [1, 1, 2, 2, 1, 3, 3, 2, 2, 1, 1, 2, 2, 2, 3, 3, 1, 2, 3];
+let drawing_area = SVGBackend::new("histogram_vertical.svg", (300, 200)).into_drawing_area();
+drawing_area.fill(&WHITE).unwrap();
+let mut chart_builder = ChartBuilder::on(&drawing_area);
+chart_builder.margin(5).set_left_and_bottom_label_area_size(20);
+let mut chart_context = chart_builder.build_cartesian_2d((1..3).into_segmented(), 0..9).unwrap();
+chart_context.configure_mesh().draw().unwrap();
+chart_context.draw_series(Histogram::vertical(&chart_context).style(BLUE.filled()).margin(10)
+    .data(data.map(|x| (x, 1)))).unwrap();
+```
+
+The result is a histogram counting the occurrences of 1, 2, and 3 in `data`:
+
+![](https://cdn.jsdelivr.net/gh/facorread/plotters-doc-data@a617d37/apidoc/histogram_vertical.svg)
+
+Here is a variation with [`Histogram::horizontal()`], replacing `(1..3).into_segmented(), 0..9` with
+`0..9, (1..3).into_segmented()`:
+
+![](https://cdn.jsdelivr.net/gh/facorread/plotters-doc-data@a617d37/apidoc/histogram_horizontal.svg)
+
+The spacing between histogram bars is adjusted with [`Histogram::margin()`].
+Here is a version of the figure where `.margin(10)` has been replaced by `.margin(20)`;
+the resulting bars are narrow and more spaced:
+
+![](https://cdn.jsdelivr.net/gh/facorread/plotters-doc-data@a617d37/apidoc/histogram_margin20.svg)
+
+[`crate::coord::ranged1d::IntoSegmentedCoord::into_segmented()`] is useful for discrete data; it makes sure the histogram bars
+are centered on each data value. Here is another variation with `(1..3).into_segmented()`
+replaced by `1..4`:
+
+![](https://cdn.jsdelivr.net/gh/facorread/plotters-doc-data@a617d37/apidoc/histogram_not_segmented.svg)
+
+[`Histogram::style()`] sets the style of the bars. Here is a histogram without `.filled()`:
+
+![](https://cdn.jsdelivr.net/gh/facorread/plotters-doc-data@a617d37/apidoc/histogram_hollow.svg)
+
+The following version uses [`Histogram::style_func()`] for finer control. Let's replace `.style(BLUE.filled())` with
+`.style_func(|x, _bar_height| if let SegmentValue::Exact(v) = x {[BLACK, RED, GREEN, BLUE][*v as usize].filled()} else {BLACK.filled()})`.
+The resulting bars come in different colors:
+
+![](https://cdn.jsdelivr.net/gh/facorread/plotters-doc-data@a617d37/apidoc/histogram_style_func.svg)
+
+[`Histogram::baseline()`] adjusts the base of the bars. The following figure adds `.baseline(1)`
+to the right of `.margin(10)`. The lower portion of the bars are removed:
+
+![](https://cdn.jsdelivr.net/gh/facorread/plotters-doc-data@a617d37/apidoc/histogram_baseline.svg)
+
+The following figure uses [`Histogram::baseline()`] for finer control. Let's add
+`.baseline_func(|x| if let SegmentValue::Exact(v) = x {*v as i32} else {0})`
+to the right of `.margin(10)`. The lower portion of the bars are removed; the removed portion is taller
+to the right:
+
+![](https://cdn.jsdelivr.net/gh/facorread/plotters-doc-data@a617d37/apidoc/histogram_baseline_func.svg)
+*/
 pub struct Histogram<'a, BR, A, Tag = Vertical>
 where
     BR: DiscreteRanged,
@@ -47,14 +107,24 @@ where
             _p: PhantomData,
         }
     }
-    /// Set the style of the histogram
+    /**
+    Sets the style of the histogram bars.
+
+    See [`Histogram`] for more information and examples.
+    */
     pub fn style<S: Into<ShapeStyle>>(mut self, style: S) -> Self {
         let style = style.into();
         self.style = Box::new(move |_, _| style.clone());
         self
     }
 
-    /// Set the style of histogram using a lambda function
+    /**
+    Sets the style of histogram using a closure.
+
+    The closure takes the position of the bar in guest coordinates as argument.
+    The argument may need some processing if the data range has been transformed by
+    [`crate::coord::ranged1d::IntoSegmentedCoord::into_segmented()`] as shown in the [`Histogram`] example.
+    */
     pub fn style_func(
         mut self,
         style_func: impl Fn(&BR::ValueType, &A) -> ShapeStyle + 'a,
@@ -63,7 +133,11 @@ where
         self
     }
 
-    /// Set the baseline of the histogram
+    /**
+    Sets the baseline of the histogram.
+
+    See [`Histogram`] for more information and examples.
+    */
     pub fn baseline(mut self, baseline: A) -> Self
     where
         A: Clone,
@@ -72,19 +146,33 @@ where
         self
     }
 
-    /// Set a function that defines variant baseline
+    /**
+    Sets the histogram bar baselines using a closure.
+
+    The closure takes the bar position and height as argument.
+    The argument may need some processing if the data range has been transformed by
+    [`crate::coord::ranged1d::IntoSegmentedCoord::into_segmented()`] as shown in the [`Histogram`] example.
+    */
     pub fn baseline_func(mut self, func: impl Fn(&BR::ValueType) -> A + 'a) -> Self {
         self.baseline = Box::new(func);
         self
     }
 
-    /// Set the margin for each bar
+    /**
+    Sets the margin for each bar, in backend pixels.
+
+    See [`Histogram`] for more information and examples.
+    */
     pub fn margin(mut self, value: u32) -> Self {
         self.margin = value;
         self
     }
 
-    /// Set the data iterator
+    /**
+    Specifies the input data for the histogram through an appropriate data iterator.
+
+    See [`Histogram`] for more information and examples.
+    */
     pub fn data<TB: Into<BR::ValueType>, I: IntoIterator<Item = (TB, A)>>(
         mut self,
         iter: I,
@@ -105,6 +193,11 @@ where
     BR: DiscreteRanged + Clone,
     A: AddAssign<A> + Default + 'a,
 {
+    /**
+    Creates a vertical histogram.
+
+    See [`Histogram`] for more information and examples.
+    */
     pub fn vertical<ACoord, DB: DrawingBackend + 'a>(
         parent: &ChartContext<DB, Cartesian2d<BR, ACoord>>,
     ) -> Self
@@ -122,6 +215,11 @@ where
     BR: DiscreteRanged + Clone,
     A: AddAssign<A> + Default + 'a,
 {
+    /**
+    Creates a horizontal histogram.
+
+    See [`Histogram`] for more information and examples.
+    */
     pub fn horizontal<ACoord, DB: DrawingBackend>(
         parent: &ChartContext<DB, Cartesian2d<ACoord, BR>>,
     ) -> Self
