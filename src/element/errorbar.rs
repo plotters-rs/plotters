4718290e04ceac54cb49591d82b5625d9fde2971
Fabio A. Correa <facorread@gmail.com>
2022-06-17 16:36:13 -0400

    ErrorBar

--- src/element/errorbar.rs
+++ src/element/errorbar.rs
@@ -4,6 +4,12 @@ use crate::element::{Drawable, PointCollection};
 use crate::style::ShapeStyle;
 use plotters_backend::{BackendCoord, DrawingBackend, DrawingErrorKind};
 
+/**
+Used to reuse code between horizontal and vertical error bars.
+
+This is used internally by Plotters and should probably not be included in user code.
+See [`ErrorBar`] for more information and examples.
+*/
 pub trait ErrorBarOrient<K, V> {
     type XType;
     type YType;
@@ -12,8 +18,20 @@ pub trait ErrorBarOrient<K, V> {
     fn ending_coord(coord: BackendCoord, w: u32) -> (BackendCoord, BackendCoord);
 }
 
+/**
+Used for the production of horizontal error bars.
+
+This is used internally by Plotters and should probably not be included in user code.
+See [`ErrorBar`] for more information and examples.
+*/
 pub struct ErrorBarOrientH<K, V>(PhantomData<(K, V)>);
 
+/**
+Used for the production of vertical error bars.
+
+This is used internally by Plotters and should probably not be included in user code.
+See [`ErrorBar`] for more information and examples.
+*/
 pub struct ErrorBarOrientV<K, V>(PhantomData<(K, V)>);
 
 impl<K, V> ErrorBarOrient<K, V> for ErrorBarOrientH<K, V> {
@@ -48,6 +66,42 @@ impl<K, V> ErrorBarOrient<K, V> for ErrorBarOrientV<K, V> {
     }
 }
 
+/**
+An error bar, which visualizes the minimum, average, and maximum of a dataset.
+
+Unlike [`crate::series::Histogram`], the `ErrorBar` code does not classify or aggregate data.
+These operations must be done before building error bars.
+
+# Examples
+
+```
+use plotters::prelude::*;
+let x_values = [1.0f64, 2., 3., 4., 5.];
+let drawing_area = SVGBackend::new("error_bars_vertical.svg", (300, 200)).into_drawing_area();
+drawing_area.fill(&WHITE).unwrap();
+let mut chart_builder = ChartBuilder::on(&drawing_area);
+chart_builder.margin(10).set_left_and_bottom_label_area_size(20);
+let mut chart_context = chart_builder.build_cartesian_2d(0.0..6.0, 0.0..6.0).unwrap();
+chart_context.configure_mesh().draw().unwrap();
+chart_context.draw_series(x_values.map(|x| {
+    let y = 4. - 0.1 * x * x;
+    ErrorBar::new_vertical(x, y - 1.0, y, y + 1.1, BLUE.filled(), 10)
+})).unwrap();
+```
+
+This code produces a series of five error bars showing minima, maxima, and average values:
+
+![](https://cdn.jsdelivr.net/gh/facorread/plotters-doc-data@413b8a1/apidoc/error_bars_vertical.svg)
+
+[`ErrorBar::new_vertical()`] is used to create vertical error bars. Here is an example using
+[`ErrorBar::new_horizontal()`] instead:
+
+![](https://cdn.jsdelivr.net/gh/facorread/plotters-doc-data@413b8a1/apidoc/error_bars_horizontal.svg)
+
+The blue color style is applied to the circles, bars, and error marks. Here is an example without `filled()`:
+
+![](https://cdn.jsdelivr.net/gh/facorread/plotters-doc-data@413b8a1/apidoc/error_bars_hollow.svg)
+*/
 pub struct ErrorBar<K, V, O: ErrorBarOrient<K, V>> {
     style: ShapeStyle,
     width: u32,
@@ -57,6 +111,18 @@ pub struct ErrorBar<K, V, O: ErrorBarOrient<K, V>> {
 }
 
 impl<K, V> ErrorBar<K, V, ErrorBarOrientV<K, V>> {
+/**
+Creates a vertical error bar.
+`
+- `key`: Horizontal position of the bar
+- `min`: Minimum of the data
+- `avg`: Average of the data
+- `max`: Maximum of the data
+- `style`: Color, transparency, and fill of the error bar. See [`ShapeStyle`] for more information and examples.
+- `width`: Width of the error marks in backend coordinates.
+
+See [`ErrorBar`] for more information and examples.
+*/
     pub fn new_vertical<S: Into<ShapeStyle>>(
         key: K,
         min: V,
@@ -76,6 +142,18 @@ impl<K, V> ErrorBar<K, V, ErrorBarOrientV<K, V>> {
 }
 
 impl<K, V> ErrorBar<K, V, ErrorBarOrientH<K, V>> {
+/**
+Creates a horizontal error bar.
+
+- `key`: Vertical position of the bar
+- `min`: Minimum of the data
+- `avg`: Average of the data
+- `max`: Maximum of the data
+- `style`: Color, transparency, and fill of the error bar. See [`ShapeStyle`] for more information and examples.
+- `width`: Width of the error marks in backend coordinates.
+
+See [`ErrorBar`] for more information and examples.
+*/
     pub fn new_horizontal<S: Into<ShapeStyle>>(
         key: K,
         min: V,
