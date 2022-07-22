058fcb5e507b974c84c4e604e9a9f967a4a92869
Hao Hou <haohou302@gmail.com>
2022-07-22 15:13:26 -0700

    Merge the evcrx_bitmap code and fix the feature

--- plotters/src/evcxr.rs
+++ plotters/src/evcxr.rs
@@ -43,36 +43,8 @@ pub fn evcxr_figure<
     SVGWrapper(buffer, "".to_string())
 }
 
-// #[cfg(all(feature = "evcxr", feature = "bitmap_backend"))]
-// pub struct BitMapWrapper(String, String);
-
-// #[cfg(all(feature = "evcxr", feature = "bitmap_backend"))]
-// impl BitMapWrapper {
-//     pub fn evcxr_display(&self) {
-//         println!("{:?}", self);
-//     }
-
-//     pub fn style<S: Into<String>>(mut self, style: S) -> Self {
-//         self.1 = style.into();
-//         self
-//     }
-// }
-
-// #[cfg(feature = "evcxr_bitmap_figure")]
-// #[cfg(all(feature = "evcxr", feature = "bitmap_backend"))]
-// impl std::fmt::Debug for BitMapWrapper {
-//     fn fmt(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
-//         let enc = self.0.as_str();
-//         write!(
-//             formatter,
-//             "EVCXR_BEGIN_CONTENT text/html\n<img style=\"{}\" src=\"data:image/png;base64,{}\"/>\nEVCXR_END_CONTENT",
-//             self.1, enc
-//         )
-//     }
-// }
-
 /// Start drawing an evcxr figure
-#[cfg(all(feature = "evcxr", feature = "bitmap_backend"))]
+#[cfg(all(feature = "evcxr_bitmap"))]
 pub fn evcxr_bitmap_figure<
     Draw: FnOnce(DrawingArea<BitMapBackend, Shift>) -> Result<(), Box<dyn std::error::Error>>,
 >(
