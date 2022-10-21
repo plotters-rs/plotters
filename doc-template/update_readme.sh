9e45ab24c82971a368e84ff76498a458b6085e69
Eval EXEC <execvy@gmail.com>
2022-10-21 18:40:00 +0800

    Fix bad link of example
    Signed-off-by: Eval EXEC <execvy@gmail.com>

--- doc-template/update_readme.sh
+++ doc-template/update_readme.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -ev
 REPO_BASE=`readlink -f $(dirname $(readlink -f $0))/../`
 ${REPO_BASE}/doc-template/render_readme.sh ${REPO_BASE}/doc-template/readme.template.md ${REPO_BASE}/doc-template/readme > ${REPO_BASE}/README.md
 
