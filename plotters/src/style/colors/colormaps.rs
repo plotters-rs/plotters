b8f5dc8a9b8025cbd99740cb92edc12db62294ae
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2023-02-25 21:23:38 +0100

    also add struct for dynamic creation of colormaps

--- plotters/src/style/colors/colormaps.rs
+++ plotters/src/style/colors/colormaps.rs
@@ -14,6 +14,96 @@ where
 }
 
 
+/// This struct is used to dynamically construct colormaps by giving it a slice of colors.
+/// It can then be used when being intantiated, but not with associated functions.
+/// ```
+/// use plotters::prelude::{BLACK,BLUE,WHITE,DerivedColorMap,ColorMap};
+/// 
+/// let derived_colormap = DerivedColorMap::new(
+///     &[BLACK,
+///     BLUE,
+///     WHITE]
+/// );
+/// 
+/// assert_eq!(derived_colormap.get_color(0.0), BLACK);
+/// assert_eq!(derived_colormap.get_color(0.5), BLUE);
+/// assert_eq!(derived_colormap.get_color(1.0), WHITE);
+/// ```
+pub struct DerivedColorMap<ColorType> {
+    colors: Vec<ColorType>,
+}
+
+
+impl<ColorType: crate::style::Color + Clone> DerivedColorMap<ColorType> {
+    pub fn new(colors: &[ColorType]) -> Self {
+        DerivedColorMap { colors: colors.iter().map(|color| color.clone()).collect::<Vec<_>>() }
+    }
+}
+
+
+macro_rules! calculate_new_color_value(
+    ($relative_difference:expr, $colors:expr, $index_upper:expr, $index_lower:expr, RGBColor) => {
+        RGBColor(
+            // These equations are a very complicated way of writing a simple linear extrapolation with lots of casting between numerical values
+            // In principle every cast should be safe which is why we choose to unwrap
+            //           (1.0  - r)                   *                                           color_value_1  +                   r *                                           color_value_2
+            ((FloatType::one() - $relative_difference) * FloatType::from_u8($colors[$index_upper].0).unwrap() + $relative_difference * FloatType::from_u8($colors[$index_lower].0).unwrap()).round().to_u8().unwrap(),
+            ((FloatType::one() - $relative_difference) * FloatType::from_u8($colors[$index_upper].1).unwrap() + $relative_difference * FloatType::from_u8($colors[$index_lower].1).unwrap()).round().to_u8().unwrap(),
+            ((FloatType::one() - $relative_difference) * FloatType::from_u8($colors[$index_upper].2).unwrap() + $relative_difference * FloatType::from_u8($colors[$index_lower].2).unwrap()).round().to_u8().unwrap()
+        )
+    };
+    ($relative_difference:expr, $colors:expr, $index_upper:expr, $index_lower:expr, RGBAColor) => {
+        RGBAColor(
+            // These equations are a very complicated way of writing a simple linear extrapolation with lots of casting between numerical values
+            // In principle every cast should be safe which is why we choose to unwrap
+            //           (1.0  - r)                   *                                           color_value_1  +                   r *                                           color_value_2
+            ((FloatType::one() - $relative_difference) * FloatType::from_u8($colors[$index_upper].0).unwrap() + $relative_difference * FloatType::from_u8($colors[$index_lower].0).unwrap()).round().to_u8().unwrap(),
+            ((FloatType::one() - $relative_difference) * FloatType::from_u8($colors[$index_upper].1).unwrap() + $relative_difference * FloatType::from_u8($colors[$index_lower].1).unwrap()).round().to_u8().unwrap(),
+            ((FloatType::one() - $relative_difference) * FloatType::from_u8($colors[$index_upper].2).unwrap() + $relative_difference * FloatType::from_u8($colors[$index_lower].2).unwrap()).round().to_u8().unwrap(),
+            ((FloatType::one() - $relative_difference) * FloatType::from_f64($colors[$index_upper].3).unwrap() + $relative_difference * FloatType::from_f64($colors[$index_lower].3).unwrap()).to_f64().unwrap()
+        )
+    };
+    ($relative_difference:expr, $colors:expr, $index_upper:expr, $index_lower:expr, HSLColor) => {
+        HSLColor(
+            // These equations are a very complicated way of writing a simple linear extrapolation with lots of casting between numerical values
+            // In principle every cast should be safe which is why we choose to unwrap
+            //           (1.0  - r)                   *                                           color_value_1  +                   r *                                           color_value_2
+            ((FloatType::one() - $relative_difference) * FloatType::from_f64($colors[$index_upper].0).unwrap() + $relative_difference * FloatType::from_f64($colors[$index_lower].0).unwrap()).to_f64().unwrap(),
+            ((FloatType::one() - $relative_difference) * FloatType::from_f64($colors[$index_upper].1).unwrap() + $relative_difference * FloatType::from_f64($colors[$index_lower].1).unwrap()).to_f64().unwrap(),
+            ((FloatType::one() - $relative_difference) * FloatType::from_f64($colors[$index_upper].2).unwrap() + $relative_difference * FloatType::from_f64($colors[$index_lower].2).unwrap()).to_f64().unwrap(),
+        )
+    };
+);
+
+
+macro_rules! implement_color_scale_for_derived_ColorMap{
+    ($($color_type:tt),+) => {
+        $(
+            impl<FloatType: Float + FromPrimitive + ToPrimitive> ColorMap<$color_type, FloatType> for DerivedColorMap<$color_type> {
+                fn get_color_normalized(&self, h: FloatType, min: FloatType, max: FloatType) -> $color_type {
+                    // Ensure that we do have a value in bounds
+                    let h = h.max(min).min(max);
+                    // Make sure that we really have a minimal value which is smaller than the maximal value
+                    assert_eq!(min<max, true);
+                    // Next calculate a normalized value between 0.0 and 1.0
+                    let t = (h - min)/(max-min);
+                    let approximate_index = t * (FloatType::from_usize(self.colors.len()).unwrap() - FloatType::one()).max(FloatType::zero());
+                    // Calculate which index are the two most nearest of the supplied value
+                    let index_lower = approximate_index.floor().to_usize().unwrap();
+                    let index_upper = approximate_index.ceil().to_usize().unwrap();
+                    // Calculate the relative difference, ie. is the actual value more towards the color of index_upper or index_lower?
+                    let relative_difference = approximate_index.ceil() - approximate_index;
+                    // Interpolate the final color linearly
+                    calculate_new_color_value!(relative_difference, self.colors, index_upper, index_lower, $color_type)
+                }
+            }
+        )+
+    }
+}
+
+implement_color_scale_for_derived_ColorMap!{RGBAColor, RGBColor, HSLColor}
+
+
 macro_rules! count {
     () => (0usize);
     ($x:tt $($xs:tt)* ) => (1usize + count!($($xs)*));
