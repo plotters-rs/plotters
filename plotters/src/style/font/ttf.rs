4ac399ecfa978ee1dc466facf49ae0e75d22f102
martinfrances107 <martinfrances107@hotmail.com>
2023-10-18 09:05:49 +0100

    Simplification, now using ??.

--- plotters/src/style/font/ttf.rs
+++ plotters/src/style/font/ttf.rs
@@ -132,16 +132,13 @@ fn load_font_data(face: FontFamily, style: FontStyle) -> FontResult<FontExt> {
     // Then we need to check if the data cache contains the font data
     let cache = DATA_CACHE.read().unwrap();
     if let Some(data) = cache.get(Borrow::<str>::borrow(&key)) {
-        let font_ext = data.clone().map(|handle| {
+        data.clone().map(|handle| {
             handle
                 .load()
                 .map(FontExt::new)
                 .map_err(|e| FontError::FontLoadError(Arc::new(e)))
-        })?;
+        })??;
 
-        if font_ext.is_err() {
-          return font_ext;
-        }
     }
     drop(cache);
 
